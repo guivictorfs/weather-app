b5af28a09107f73b5176afc8813ec3eaaaa0bc9f
Gui Victor <guivictorfs@hotmail.com>
2024-02-20 20:30:46 -0300

    update dependencies

---
+++ src/helpers/initialData.js
@@ -0,0 +1,16 @@
+const initialData = {
+  location: {
+    name: 'City',
+    region: 'State',
+    country: 'Country',
+  },
+  current: {
+    temp_c: 0,
+    condition: {
+      text: 'Weather Condition',
+      icon: '//cdn.weatherapi.com/weather/64x64/night/116.png',
+    },
+  },
+};
+
+export default initialData;
---
+++ src/services/api.js
@@ -0,0 +1,12 @@
+const KEY = 'a156dc82dc814b8eacc183520241902';
+
+const fetchData = async (city) => {
+  const url = `http://api.weatherapi.com/v1/current.json?key=${KEY}&q=${city}&aqi=no`;
+
+  const fetchResponse = await fetch(url);
+  const data = await fetchResponse.json();
+
+  return data;
+};
+
+export default fetchData;
